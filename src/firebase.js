adcbade6aa67f7a910cd340cf2393295284e3f27
vivek garg <48766020+vgarg6342@users.noreply.github.com>
2020-07-08 10:37:55 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import * as firebase from 'firebase/app'
 import 'firebase/firestore'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDCEfaqoCzn-9-lhcWOCc7FkNrXZRincPk",
-    authDomain: "react-app-try1.firebaseapp.com",
-    databaseURL: "https://react-app-try1.firebaseio.com",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
     projectId: "react-app-try1",
     storageBucket: "react-app-try1.appspot.com",
-    messagingSenderId: "1099064623644",
-    appId: "1:1099064623644:web:a6e1ef60742ff80e7395d9",
-    measurementId: "G-JQN8LN85M8"
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 }
 firebase.initializeApp(firebaseConfig);
 
